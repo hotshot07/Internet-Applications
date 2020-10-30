c27e42edd3407e0fe59c5d59961f04e618a535dd
hotshot07 <aroram@tcd.ie>
2020-10-30 05:42:20 +0000

    added convert

--- weather-app/server.js
+++ weather-app/server.js
@@ -13,18 +13,59 @@ app.get('/api/data/:location',(req,res)=>{
 
     let url = `https://api.openweathermap.org/data/2.5/forecast?q=${req.params.location}&appid=${API_KEY}&units=metric`
     
+    let data = null
+
     axios.get(url)
         .then(function (response) {
-                res.send(response.data);
+                
+                var data = response.data;
+                final_data = convert(data);
+                res.send(data);
+
         })
         .catch(function (error) {
-                res.send(JSON.stringify({ error: "City does not exist"}))
+                console.log(error)
+                res.send(JSON.stringify({error : "City does not exist"}))
         })
 
 
 });
 
+function convert(data){
+        var list_of_dates  = [];
 
+        for (date of data["list"]){
+                list_of_dates.push(date["dt_txt"].slice(0,10))
+        }
+
+        var unique_dates = [... new Set(list_of_dates)]
+
+        for (i=0;i<unique_dates.length;i++){
+
+                var cur_date = unique_dates[i];
+                let cur_temp_min = [];
+                let cur_temp_max = [];
+                let wind_speed = [];
+
+                var chance_of_rain = false
+
+                for(main_data of data["list"]) {
+                        if(cur_date === main_data["dt_txt"].slice(0,10)){
+                                cur_temp_min.push(main_data["main"]["temp_min"])
+                                cur_temp_max.push(main_data["main"]["temp_max"])
+
+                        }
+
+                }
+
+                console.log(cur_date);
+                console.log(Math.min(...cur_temp_min));
+                console.log(Math.max(...cur_temp_max));
+                console.log( );
+        }
+
+        return data
+}
 
 
 
