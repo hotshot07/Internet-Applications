e32638c5408b8ae354757c1616afe6f0ce5463ba
hotshot07 <aroram@tcd.ie>
2020-10-31 05:30:25 +0000

    starting beautifying

--- weather-app/server.js
+++ weather-app/server.js
@@ -9,34 +9,34 @@ app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
 
 API_KEY = '59c3c3690cc24e1ecda415ebe6b31871';
 
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
+app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
 });
 
 
-app.get('/api/data/:location',(req,res)=>{
+app.get('/api/data/:location', (req, res) => {
 
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${req.params.location}&appid=${API_KEY}&units=metric`
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${req.params.location}&appid=${API_KEY}&units=metric`
 
-    axios.get(url)
-        .then(function (response) {
+        axios.get(url)
+                .then(function (response) {
 
-                var data = response.data;
-                let final_data = convert(data);
-                res.send(final_data);
+                        var data = response.data;
+                        let final_data = convert(data);
+                        res.send(final_data);
 
-        })
-        .catch(function (error) {
-                console.log(error)
-                res.send(JSON.stringify({ error : "city does not exist"}));
-        })
+                })
+                .catch(function (error) {
+                        console.log(error)
+                        res.send(JSON.stringify({ error: "city does not exist" }));
+                })
 
 
 });
 
-function convert(data){
+function convert(data) {
 
         final_data = {
                 city: data.city.name,
@@ -45,10 +45,10 @@ function convert(data){
         }
 
 
-        var list_of_dates  = [];
+        var list_of_dates = [];
 
-        for (date of data["list"]){
-                list_of_dates.push(date["dt_txt"].slice(0,10))
+        for (date of data["list"]) {
+                list_of_dates.push(date["dt_txt"].slice(0, 10))
         }
 
         var unique_dates = [... new Set(list_of_dates)]
@@ -56,7 +56,7 @@ function convert(data){
         var global_min = Infinity;
         var global_max = -Infinity;
 
-        for (i=0;i<unique_dates.length;i++){
+        for (i = 0; i < unique_dates.length; i++) {
 
                 let weather_object = {}
 
@@ -64,23 +64,23 @@ function convert(data){
                 let cur_temp_min = [];
                 let cur_temp_max = [];
                 let wind_speed = [];
-                
+
                 var weather_type = null
                 var rain_object = {}
 
 
                 var chance_of_rain = false
 
-                for(main_data of data["list"]) {
-                        if(cur_date === main_data["dt_txt"].slice(0,10)){
+                for (main_data of data["list"]) {
+                        if (cur_date === main_data["dt_txt"].slice(0, 10)) {
 
                                 cur_temp_min.push(main_data["main"]["temp_min"])
                                 cur_temp_max.push(main_data["main"]["temp_max"])
                                 weather_type = main_data.weather[0].main
 
-                                if(weather_type === "Rain"){
+                                if (weather_type === "Rain") {
                                         chance_of_rain = true;
-                                        rain_object[main_data["dt_txt"].slice(11,19)] = main_data["rain"]["3h"]
+                                        rain_object[main_data["dt_txt"].slice(11, 19)] = main_data["rain"]["3h"]
                                 }
 
                                 wind_speed.push(main_data["wind"]["speed"])
@@ -89,7 +89,7 @@ function convert(data){
 
                 }
 
-                
+
                 min_temp = Math.min(...cur_temp_min)
                 max_temp = Math.max(...cur_temp_max)
 
@@ -100,43 +100,43 @@ function convert(data){
                 //weather_object["weather_type"] = weather_type
                 weather_object["rain"] = chance_of_rain
 
-                if(chance_of_rain){
+                if (chance_of_rain) {
                         weather_object["expected_rainfall"] = rain_object;
                 }
 
                 final_data.weather.push(weather_object)
 
-                if(min_temp < global_min){
+                if (min_temp < global_min) {
                         global_min = min_temp;
                 }
 
-                if(max_temp>global_max){
+                if (max_temp > global_max) {
                         global_max = max_temp;
                 }
 
         }
 
-        final_data["max_in_week"] =  Math.ceil(global_max);
-        final_data["min_in_week"] =  Math.floor(global_min);
+        final_data["max_in_week"] = Math.ceil(global_max);
+        final_data["min_in_week"] = Math.floor(global_min);
 
-        let avg_temp = (Math.ceil(global_max)+ Math.floor(global_min))/2;
+        let avg_temp = (Math.ceil(global_max) + Math.floor(global_min)) / 2;
 
         final_data["average_temp"] = avg_temp;
 
-        if(avg_temp > -10 && avg_temp <= 10){
+        if (avg_temp > -10 && avg_temp <= 10) {
                 final_data["packing"] = "cold";
-        }else if (avg_temp >10  && avg_temp <=20){
+        } else if (avg_temp > 10 && avg_temp <= 20) {
                 final_data["packing"] = "warm";
-        }else if (avg_temp >20){
+        } else if (avg_temp > 20) {
                 final_data["packing"] = "hot";
-        }else{
+        } else {
                 final_data["packing"] = "extreme_cold";
         }
 
         final_data["expected_rain"] = false;
 
-        for(data of final_data.weather){
-                if(data.rain){
+        for (data of final_data.weather) {
+                if (data.rain) {
                         final_data["expected_rain"] = true;
                 }
         }
